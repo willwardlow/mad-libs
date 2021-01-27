b6b2a8b6f6592206d57b26a90897bf9e29c3588b
willwardlow <wardlow.william@gmail.com>
2021-01-27 13:38:06 -0600

    Completed prompts for user

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const getTemplate = async () => {
   try {
     const response = await axios.get(url)
     const play = document.querySelector('#start')
-    enterWords(response.data.blanks)
-    console.log(response.data.blanks)
+    // enterWords(response.data.blanks, 0)
+    let story = writeStory(response.data.value, response.data.blanks)
+    console.log(response)
   } catch (error) {
     console.log(error)
   }
@@ -21,36 +22,37 @@ getTemplate()
 
 
 
+let fillIn = []
 
-
-function enterWords(prompts) {
+function enterWords(prompts, i) {
   // fillIn: array of user choices
-  let fillIn = []
+
 
   // Capturing DOM elements
   const submit = document.querySelector('#submit')
   let answer = document.querySelector('#entries')
   let request = document.querySelector('#prompt')
-  let form = document.querySelector('#player-entries')
   // Creating forEach loop to cycle through prompts received from API and capturing user's responses. The responses will be pushed into an array.
-  prompts.forEach((item) => {
-    request.innerHTML = `${item} :`
-    submit.onclick = (e) => {
-      e.preventDefault()
-      if (answer.value.length === 0) {
-        console.log('Please enter a word')
-      } else {
-        fillIn.push(answer.value)
-      }
-      // Resetting values before moving on to next item
-      answer.value = ''
-      console.log(fillIn)
-      console.log(prompts)
-    }
-    
 
-  })
-    
+  request.innerHTML = `${prompts[i]} :`
+  submit.onclick = (e) => {
+    e.preventDefault()
+    console.log('here', fillIn, i)
+    if (answer.value.length === 0) {
+      console.log('Please enter a word')
+    } else {
+      fillIn.push(answer.value)
+      enterWords(prompts, i += 1)
+    }
+    // Resetting values before moving on to next item
+    answer.value = ''
+    console.log(prompts)
+    console.log(fillIn)
+  }
+
+
 }
 
-// play.onclick = play(response.data.blanks)
+function writeStory(template, words) {
+  const story = template.join(words)
+}
